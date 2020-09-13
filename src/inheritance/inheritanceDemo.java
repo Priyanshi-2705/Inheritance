4353b2b703a4efb1c5817f1097a649247374e2cb
Lenovo <priyanshi.jaiswal_cs19@gla.ac.in>
2020-09-13 16:00:32 +0530

    Initial commit

---
+++ src/inheritance/inheritanceDemo.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Priyanshi Jaiswal
+ *   Date: 13-09-2020
+ *   Time: 15:57
+ *   File: inheritanceDemo.java
+ */
+
+package inheritance;
+
+public class inheritanceDemo {
+}
