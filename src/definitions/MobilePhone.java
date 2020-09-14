6ba0ed3ccd0fd9b775d72b537ee926da1ca98e89
Lenovo <priyanshi.jaiswal_cs19@gla.ac.in>
2020-09-14 17:21:49 +0530

    create MobilePhone.java

---
+++ src/definitions/MobilePhone.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Priyanshi Jaiswal
+ *   Date: 14-09-2020
+ *   Time: 17:17
+ *   File: MobilePhone.java
+ */
+
+package definitions;
+
+public class MobilePhone {
+
+}
