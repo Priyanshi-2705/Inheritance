f06d37a8c6f465c84f4e7de9797dafa77c2202fe
Lenovo <priyanshi.jaiswal_cs19@gla.ac.in>
2020-09-14 17:37:46 +0530

    create Main.javawith example of importing the Scanner class

---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Priyanshi Jaiswal
+ *   Date: 14-09-2020
+ *   Time: 17:30
+ *   File: Main.java
+ */
+
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
+    }
+}
